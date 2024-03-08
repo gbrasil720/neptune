aeeee67d136b8ec743f2c61f1ed31355e7666713
Guilherme Brasil <dev.guilhermebrasil@gmail.com>
2024-03-08 11:31:09 -0300

    feat: add fastify-cors

--- api/src/http/server.ts
+++ api/src/http/server.ts
@@ -1,20 +1,29 @@
-import fastify from 'fastify'
-import chalk from 'chalk'
+import fastify from "fastify";
+import chalk from "chalk";
 
-import { createUser } from './routes/create-user'
-import { getUsers } from './routes/get-users'
-import { getSpecificUser } from './routes/get-specific-user'
-import { updateUserRole } from './routes/update-user-role'
-import { deleteUser } from './routes/delete-user'
+import { createUser } from "./routes/create-user";
+import { getUsers } from "./routes/get-users";
+import { getSpecificUser } from "./routes/get-specific-user";
+import { updateUserRole } from "./routes/update-user-role";
+import { deleteUser } from "./routes/delete-user";
+import fastifyCors from "@fastify/cors";
 
-const app = fastify()
+const app = fastify();
 
-app.register(createUser)
-app.register(getUsers)
-app.register(getSpecificUser)
-app.register(updateUserRole)
-app.register(deleteUser)
+app.register(fastifyCors, {
+  origin: "*",
+});
+
+app.register(createUser);
+app.register(getUsers);
+app.register(getSpecificUser);
+app.register(updateUserRole);
+app.register(deleteUser);
 
 app.listen({ port: 3333 }).then(() => {
-  console.log(`🚀 ${chalk.greenBright('[API] Started running successfully at http://localhost:3333')}`)
-})
+  console.log(
+    `🚀 ${chalk.greenBright(
+      "[API] Started running successfully at http://localhost:3333"
+    )}`
+  );
+});
